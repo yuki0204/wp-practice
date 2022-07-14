f3421f49cc03266eedf27989f0f741bba29ebd46
yuki0204 <badsketball07@gmail.com>
2022-07-14 22:19:54 +0900

    軽量化対応 ・不要なプラグインの削除 ・アップロード画像の圧縮 など

--- plugins/show-current-template/assets/js/replace.js
+++
@@ -1,7 +0,0 @@
-function replaceFileNames() {
-	let c = document.getElementById('included-files-fie-on-wp-footer').innerHTML;
-	document.getElementById('included-files-list').innerHTML = c;
-}
-jQuery( function() { 
-	replaceFileNames();
-});
